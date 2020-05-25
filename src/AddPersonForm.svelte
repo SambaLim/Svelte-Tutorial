56d1b31d35d46b299f501feb68b1fbfe0aa7f84f
Sambalim <limsungho07@hanmail.net>
2020-05-25 23:53:09 +0900

    16. Forms part2

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,16 +2,29 @@
   let name;
   let beltColor;
   let age;
+  let skills = [];
 
   const handleSubmit = () => {
-    console.log(name, beltColor, age);
+    console.log(name, beltColor, age, skills);
   }
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
   <input type="text" placeholder="name" bind:value={name}>
-  <input type="text" placeholder="belt color" bind:value={beltColor}>
   <input type="number" placeholder="age" bind:value={age}>
+  
+  <label>Skills:</label>
+  <input type="checkbox" bind:group={skills} value="fighting">fighting
+  <input type="checkbox" bind:group={skills} value="sneaking">sneaking
+  <input type="checkbox" bind:group={skills} value="running">running
+
+  <label>Belt color:</label>
+  <select bind:value={beltColor}>
+    <option value="black">black</option>
+    <option value="orange">orange</option>
+    <option value="brown">brown</option>
+    <option value="white">white</option>
+  </select>
   <button>Add Person</button>
 </form>
 
